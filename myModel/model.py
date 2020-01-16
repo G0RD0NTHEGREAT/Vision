b66714933d987308fc72cebe2eb40a3e9a7712d0
Gordon Xu <gordonxyh@gmail.com>
2020-01-16 14:43:23 -0500

    vars to GPU

--- myModel/model.py
+++ myModel/model.py
@@ -670,9 +670,12 @@ class DVSA(torch.nn.Module):
 
         # d_ti_n (Na, Ns, Nb, Nd)  ,  sim_mat (Na, Ns, Nd, Na, Ne)
         # Knowledge_sim (Na,Ns,Nb,Na,Ne)
+
         sim_mat_ = sim_mat.unsqueeze(dim=2)                 # sim_mat_  (Na, Ns, 1 , Nd, Na, Ne)
         d_ti_n_ = d_ti_n.unsqueeze(dim=4).unsqueeze(dim=4) # d_ti_n_   (Na, Ns, Nb, Nd, 1, 1)
-        
+        sim_mat_.to(sim_mat.device)
+        d_ti_n_.to(sim_mat.device)
+        Knowledge_sim.to(sim_mat.device)
         Knowledge_sim = (sim_mat_ * d_ti_n_).max(dim=3)
         print('Na: {}, Ns: {}, Nb: {}, Na: {}, Ne: {}'.format(Na, Ns, Nb, Na, Ne))
         print('shape of Knowledge_sim: {}'.format(Knowledge_sim.size()))
