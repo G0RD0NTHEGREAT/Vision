5cbb61cd84c97f7e89dc47abb76edca76081452e
Gordon Xu <gordonxyh@gmail.com>
2020-01-21 16:46:26 -0500

    test

--- myModel/model.py
+++ myModel/model.py
@@ -733,9 +733,9 @@ class DVSA(torch.nn.Module):
         for na in range(Na):
             for ns in range(Ns):
                 for nb in range(Nb):
-                    #d_ti_n[na,ns,nb,:] = self.IOU_fast(boxes[na*Ns*Nb+ns*Nb+nb].cpu().numpy(), DetectBox_[na*Ns*maxLen+ns*maxLen : na*Ns*maxLen+ns*maxLen + maxLen].cpu().numpy())
-                    for nd in range(maxLen):
-                        d_ti_n[na,ns,nb,nd] = self.IOU(boxes[na*Ns*Nb+ns*Nb+nb], DetectBox_[na*Ns*maxLen+ns*maxLen+nd])
+                    d_ti_n[na,ns,nb,:] = self.IOU_fast(boxes[na*Ns*Nb+ns*Nb+nb].cpu().numpy(), DetectBox_[na*Ns*maxLen+ns*maxLen : na*Ns*maxLen+ns*maxLen + maxLen].cpu().numpy())
+                    # for nd in range(maxLen):
+                    #     d_ti_n[na,ns,nb,nd] = self.IOU(boxes[na*Ns*Nb+ns*Nb+nb], DetectBox_[na*Ns*maxLen+ns*maxLen+nd])
                     
 
                     # print("faster")
